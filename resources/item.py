84ec91a1f853e181c32857ee9576e4aa2b299ede
Thabo.Ngara <th.kuzin@gmail.com>
2024-11-10 08:31:11 +0000

    updated store and item using database SQLAlchemy

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,8 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from schemas import ItemSchema, ItemUpdateSchema
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db # importing sql alchemy database
 from models import ItemModel # relating to database 
 
 blp = Blueprint("item", __name__, description="Operations on items")
@@ -12,32 +14,21 @@ class Item(MethodView):
     # Get Items 
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id) # checks for data and manages error handling get_or_404
+        return item
 
     # Delete Items
     def delete_item(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="item not found.")
+        item = ItemModel.query.get_or_404(item_id) # checks for data and manages error handling get_or_404
+        raise NotImplementedError("Deleting an item is not implmented")
 
     # Update Items
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
+       item = ItemModel.query.get_or_404(item_id) # checks for data and manages error handling get_or_404
+       raise NotImplementedError("Updating an item is not implmented")
 
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
 
 
 @blp.route("/item")
@@ -49,16 +40,13 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema) # From Mashmallow by importing schemas
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        item = ItemModel(**item_data)
+        # Need to include rollback if there is an error
+        try:    
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error has occured while inserting data to the database")
 
         return item
 
--- resources/store.py
+++ resources/store.py
@@ -1,8 +1,13 @@
-import uuid
+#import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+
 from schemas import StoreSchema
+from db import db # importing sql alchemy database
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from models import StoreModel # relating to database 
+
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
@@ -10,17 +15,12 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Stores(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        return store
     
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
             
 @blp.route("/store")
 class StoreList(MethodView):
@@ -31,16 +31,12 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
+        store = StoreModel(**store_data)
 
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(
-                    400, 
-                    message=f"Store already exists"
-                )
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error has occured while inserting data to the database")
 
         return store
